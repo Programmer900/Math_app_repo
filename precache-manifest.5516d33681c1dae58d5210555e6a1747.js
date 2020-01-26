e7bd8b073c3c642a995c4463fa04c490a14f1224
Programmer900 <IONIZATOR231@yandex.ru>
2020-01-26 22:22:28 +0700

    Add files via upload

---
+++ precache-manifest.5516d33681c1dae58d5210555e6a1747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a890f9400d0a5277d18",
+    "url": "css/app.8c9d96a0.css"
+  },
+  {
+    "revision": "9a890f9400d0a5277d18",
+    "url": "js/app.3de9a86d.js"
+  },
+  {
+    "revision": "ac6e20312505b5f49eb6",
+    "url": "css/chunk-vendors.fa9cb4d8.css"
+  },
+  {
+    "revision": "ac6e20312505b5f49eb6",
+    "url": "js/chunk-vendors.619ff769.js"
+  },
+  {
+    "revision": "3c4764392a1762f6d05981008a8423b8",
+    "url": "index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
